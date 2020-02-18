a34d21752533ae8ec422ab39353915552227fa7b
Flaxen <alexander.carlsson@live.se>
2020-02-18 15:18:42 +0100

    added skeleton for HTTPAsk based on Echo

--- task3/HTTPAsk.java
+++ task3/HTTPAsk.java
@@ -1,9 +1,44 @@
 import java.net.*;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class HTTPAsk {
-    public static void main( String[] args) {
+    public static void main( String[] args) throws IOException {
         // Your code here
+        int portNumber = Integer.parseInt(args[0]);
+
+        System.out.println("creating host on port: " + portNumber);
+
+        ServerSocket server = new ServerSocket(portNumber);
+
+        while(true) {
+          System.out.println("Awaiting Client");
+          Socket socket = server.accept();
+          System.out.println("Client accepted");
+
+          BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+          String outputString = "";
+          String lastLine = in.readLine();
+
+          while(lastLine != null) {
+            System.out.println("LastLine: " + lastLine);
+            if(lastLine.equals("")) {
+              break;
+            }
+
+            outputString = outputString + lastLine + "\r\n";
+            lastLine = in.readLine();
+          }
+
+          outputString = "HTTP/1.1 200 OK\r\n\r\n" + outputString;
+
+          byte[] output = outputString.getBytes(StandardCharsets.UTF_8);
+
+          OutputStream out = socket.getOutputStream();
+          out.write(output);
+
+          socket.close();
+
+        }
     }
 }
-
